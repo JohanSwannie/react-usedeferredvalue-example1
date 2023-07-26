4b827d60ae58e9d00eb102df03f78e9a1185df3d
jswan <jswannieman@gmail.com>
2023-07-26 15:44:01 +1200

    Update App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React, { useState, useDeferredValue } from "react";
+import React, { useState } from "react";
+import List from "./List";
 
 function App() {
-  return <div className="App"></div>;
+  const [inputData, setInputData] = useState("");
+  return (
+    <div className="App">
+      <input
+        type="text"
+        onChange={(event) => setInputData(event.target.value)}
+      />
+      <ul>
+        <List inputData={inputData} />
+      </ul>
+    </div>
+  );
 }
 
 export default App;
